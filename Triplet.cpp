0a62ad3f4675a1c205fae01c7a5eca02f9466c2f
Raza-neduet <raza.neduet28@gmail.com>
2025-04-19 11:56:50 +0500

    Triplet

---
+++ Triplet.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main(){
+
+int a[3];
+int b[3];
+for(int i=0 ;i<3 ;i++){
+    cin>>a[i];
+}
+for(int i=0 ;i<3 ;i++){
+    cin>>b[i];
+}
+    int count=0;
+            int passcount=0;
+for(int i=0 ;i<3 ;i++){
+    if(a[i]>b[i]){
+
+        count=count+1;
+
+    }
+    if(a[i]<b[i]){
+
+     passcount=passcount+1;
+
+    }
+    else{
+        cout<<" ";
+    }
+
+}
+   cout<<passcount;
+   cout<<count;
+}
+
+

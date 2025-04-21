0c79060628655e837296ba16e14dcb2bcc5cf85d
Raza-neduet <raza.neduet28@gmail.com>
2025-04-21 15:26:21 +0500

    Number Line Jump

---
+++ NumberLineJump.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+int x1,x2,v1,v2;
+cin>>x1>>v1;
+cin>>x2>>v2;
+string kangaroo;
+int x,y;
+for(int i=1;i<1000000;i++){
+        x=x1+(v1*i);
+        y=x2+(v2*i);
+  if(x==y){
+        cout<<"YES";
+    }
+
+}
+
+cout<< "NO";
+}
+
